f352ec4aa7372b01455ffc4f8d9e8f2b0f0aed69
wanghao <wanghao@youbangsoft.com>
2018-06-04 17:41:29 +0800

    update

---
+++ src/main/java/com/haozi/jdkread/StringDemo.java
@@ -0,0 +1,12 @@
+package com.haozi.jdkread;
+
+/**
+ * @author wanghao
+ * @Description
+ * @date 2018-06-04 16:44
+ */
+public class StringDemo {
+    public static String newString() {
+        return new String("abcdefg");
+    }
+}
---
+++ src/test/java/com/haozi/jdkread/StringDemoTest.java
@@ -0,0 +1,13 @@
+package com.haozi.jdkread;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Administrator on 2018/6/4 0004.
+ */
+public class StringDemoTest extends TestCase {
+    public void testNewString() throws Exception {
+
+    }
+
+}
