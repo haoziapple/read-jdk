d860198f023989eb8a7357048ae21cf699aae70e
haoziapple <haozixiaowang@163.com>
2018-07-05 20:28:20 +0800

    add vector code

--- src/main/java/com/haozi/jdkread/list/VectorCode.java
+++ src/main/java/com/haozi/jdkread/list/VectorCode.java
@@ -1,6 +1,7 @@
 package com.haozi.jdkread.list;
 
 import java.util.Enumeration;
+import java.util.ListIterator;
 import java.util.Vector;
 
 public class VectorCode {
@@ -12,12 +13,24 @@ public class VectorCode {
         System.out.println(vector.toString());
 
         // 使用elements遍历
+        System.out.println("===使用elements遍历===");
         Enumeration enumeration = vector.elements();
         //因为使用了内部类
         //class java.util.Vector$1
         System.out.println(enumeration.getClass());
         while (enumeration.hasMoreElements()) {
             System.out.println(enumeration.nextElement());
+        }
+
+        // 使用Listiterator遍历
+        System.out.println("===使用Listiterator遍历===");
+        ListIterator listIterator = vector.listIterator();
+        while (listIterator.hasNext()) {
+            System.out.println(listIterator.next());
+        }
+        listIterator = vector.listIterator(vector.size());
+        while (listIterator.hasPrevious()) {
+            System.out.println(listIterator.previous());
         }
 
     }
